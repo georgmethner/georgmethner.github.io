f6a1b59b1810740693b67e3a47209f6a102dde14
McPeverick <georg.methner@gmail.com>
2023-02-28 12:01:42 +0100

    start

---
+++ index.js
@@ -0,0 +1,15 @@
+function card1press()
+{
+    document.getElementById('card1').id = 'card1press';
+}
+
+function card2press()
+{
+    document.getElementById('card2').id = 'card2press';
+}
+
+function card3press()
+{
+    document.getElementById("card3").style.marginLeft = "calc(50% - 140vw)";
+
+}
